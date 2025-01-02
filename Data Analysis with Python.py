0d5d392faa2959fae961fbb97c38f6039a211481
Machakrabo <maloshree.mukherjee@gmail.com>
2025-01-02 17:52:53 +0100

    UpdateCodewithdataplot

--- Data Analysis with Python.py
+++ Data Analysis with Python.py
@@ -9,5 +9,19 @@ import seaborn as sns
 url = "https://raw.githubusercontent.com/Machakrabo/Python-projects/main/automobileEDA.csv"
 df = pd.read_csv(url)
 print(df.head())
+#display only the first rows
 print(df.info())
+#Are values missing?
 print(df.isnull().sum())
+#Find the correlation between the following columns: bore, stroke, compression-ratio, and horsepower.
+# Ensure relevant columns are numeric
+#columns_to_convert = ["bore", "stroke", "compression-ratio", "horsepower"]
+#df[columns_to_convert] = df[columns_to_convert].apply(pd.to_numeric, errors='coerce')
+Matrix_Corr=df[["bore", "stroke", "compression-ratio", "horsepower"]].corr()
+print(Matrix_Corr)
+# Visualization
+sns.regplot(x="engine-size", y="price", data=df)
+plt.xlabel("Engine Size")
+plt.ylabel("Price")
+plt.title("Relationship between Engine Size and Price")
+plt.show()
