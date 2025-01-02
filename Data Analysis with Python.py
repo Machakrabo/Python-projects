2f63790279a338eaef09ad71da5910e4cf2782dd
Machakrabo <maloshree.mukherjee@gmail.com>
2025-01-02 17:40:53 +0100

    Updating the correct code

--- Data Analysis with Python.py
+++ Data Analysis with Python.py
@@ -6,11 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 #setting up the location of the file and the columns and reading the file.
-path ="automobileEDA.csv"
-df = pd.read_csv(path)
+url = "https://raw.githubusercontent.com/Machakrabo/Python-projects/main/automobileEDA.csv"
+df = pd.read_csv(url)
 print(df.head())
 print(df.info())
 print(df.isnull().sum())
-#cols=["symboling", "normalized-losses","make","aspiration","num-of-doors","body-style","drive-wheels","engine-location",
-      #"wheel-base","length","width","height","curb-weight","engine-type","num-of-cylinders","engine-size","fuel-system","bore","stroke","compression-ratio","horsepower","peak-rpm","city-mpg","highway-mpg",
-      #"price","city-L/100km","horsepower-binned","diesel","gas"]
